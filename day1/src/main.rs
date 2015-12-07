f31db3412ee7cf16666c324fddaaba7ba90cc66f
Flaeme Flow <flaeme.git@flae.me>
2015-12-07 20:45:52 +0000

    Day 1 complete

---
+++ day1/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() {
+    let program = include_str!("input.txt");
+    let mut floor : i32 = 0;
+    for c in program.char_indices() {
+        match c {
+            (_,'(') => floor += 1,
+            (_,')') => floor -= 1,
+            _ => panic!("Invalic character")
+        }
+        match c {
+            (step, _) => {
+                if floor == -1 {
+                    println!("Entered first basement on step {}", step+1);
+                }
+            }
+        }
+    }
+    println!("{}", floor);
+}
