f35f08b8e1aa2d78aed5a6d9d6efb25a75528659
Myst Fox <myst@focks.pw>
2017-12-01 22:42:06 -0700

    Day 1 part 1 solved!

--- 2017/day1/src/day1/core.clj
+++ 2017/day1/src/day1/core.clj
@@ -1,7 +1,25 @@
 (ns day1.core
   (:gen-class))
 
+(defn zip
+  "A quick zip/zipWith implementation"
+  [& colls]
+  (partition (count colls) (apply interleave colls)))
+
+(defn matchsum
+  "Finds the sum of all digits that match the next digit in a string.
+  (It \"wraps\" around, so that the last digit must match the first to be added)"
+  [x]
+  (->> x
+       seq
+       (map (fn [^Character c] (Character/digit c 10)))
+       ((fn [y] (zip y (next (cycle y)))))
+       (map (fn [v] (if (= (first v) (second v)) (first v) 0)))
+       ;; Add things that do the logic here
+       (reduce +)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Lets you run matchsum from the command line."
   [& args]
-  (println "Hello, World!"))
+  (let [file (if args (first args) "input")]
+    (println (matchsum (clojure.string/trim (slurp file))))))
